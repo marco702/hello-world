d8ee3688fcf3bcb07b77ec89c332e027191a1aad
marco702 <69803256+marco702@users.noreply.github.com>
2020-08-28 14:23:39 +0200

    Delete ChineseChef.py

--- ChineseChef.py
+++
@@ -1,9 +0,0 @@
-from Chef import Chef
-
-class ChineseChef(Chef):
-
-    def make_fried_chicken(self):
-        print("Fried chicken is yummy")
-
-    def make_pizza(self):
-        print("Pizza is not as good as from the italian chef though")
